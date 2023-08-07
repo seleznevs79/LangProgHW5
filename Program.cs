6758766a45a01908dfed34b7f17b652d3e42e700
Serg <seleznevs79@mail.ru>
2023-08-07 11:23:41 +0700

    2 задача дз5

--- Program.cs
+++ Program.cs
@@ -2,19 +2,46 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-int[] array = GetArray(10, 0, 1000);
+// int[] array = GetArray(10, 0, 1000);
+// Console.WriteLine($"[{String.Join(",", array)}]");
+
+// int chetSum = 0;
+// for (int i = 0; i < array.Length; i++){
+//     if (array[i] %2 ==0){
+//         chetSum = chetSum +1;
+//     }
+// }
+
+// Console.WriteLine($"Количество четных чисел в массиве = {chetSum}");
+
+//  // Общий метод для создания одномерного массива
+// int[] GetArray(int size, int minValue, int maxValue){
+// int[] result = new int[size];
+// for(int i = 0; i < size; i++){
+// result[i] = new Random().Next(minValue, maxValue + 1);
+// }
+// return result;
+// }
+
+
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
+//  стоящих на нечётных индексах.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = GetArray(5, 0, 20);
 Console.WriteLine($"[{String.Join(",", array)}]");
 
-int chetSum = 0;
-for (int i = 0; i < array.Length; i++){
-    if (array[i] %2 ==0){
-        chetSum = chetSum +1;
-    }
+int nechetSum = 0;
+for (int i = 0; i < array.Length; i= i+2){
+    nechetSum = nechetSum + array[i];
 }
 
-Console.WriteLine($"Количество четных чисел в массиве = {chetSum}");
+Console.WriteLine($"Количество четных чисел в массиве = {nechetSum}");
 
- // Общий метод для создания одномерного массива
+// Общий метод для создания одномерного массива
 int[] GetArray(int size, int minValue, int maxValue){
 int[] result = new int[size];
 for(int i = 0; i < size; i++){
@@ -25,11 +52,6 @@ return result;
 
 
 
-
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
-//  стоящих на нечётных индексах.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
 
 // Задача 38: Задайте массив целых чисел. Найдите разницу между максимальным и минимальным
 //  элементов массива.
